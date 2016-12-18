852513a75cee3bdc96f0c092128618333d7886a3
tomsloni <aiki.waw@gmail.com>
2016-12-18 21:15:22 +0100

    update

---
+++ chuck_jquery.js
@@ -0,0 +1,20 @@
+﻿var url = 'http://api.icndb.com/jokes/random';
+
+var $button = $('#get-joke').click(function() {
+	getJoke();
+});
+
+var $paragraph = $('#joke');
+
+function getJoke() {
+	$.ajax({
+		method: 'GET',
+		url: url,
+		success: function(res) {
+			$paragraph.text(res.value.joke)
+		}
+	});
+}
+
+
+$(function(){ getJoke(); });
