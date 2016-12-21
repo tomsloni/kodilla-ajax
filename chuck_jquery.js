6b25515cc2bbe7e0201b1eb6b1c584b02a732a98
tomsloni <aiki.waw@gmail.com>
2016-12-21 01:45:48 +0100

    changes

--- chuck_jquery.js
+++ chuck_jquery.js
@@ -1,20 +1,21 @@
-﻿var url = 'http://api.icndb.com/jokes/random';
+﻿$(function(){ 
+	var url = 'http://api.icndb.com/jokes/random';
 
-var $button = $('#get-joke').click(function() {
-	getJoke();
-});
-
-var $paragraph = $('#joke');
-
-function getJoke() {
-	$.ajax({
-		method: 'GET',
-		url: url,
-		success: function(res) {
-			$paragraph.text(res.value.joke)
-		}
+	var $button = $('#get-joke').click(function() {
+		getJoke();
 	});
-}
 
+	var $paragraph = $('#joke');
 
-$(function(){ getJoke(); });
+	function getJoke() {
+		$.ajax({
+			method: 'GET',
+			url: url,
+			success: function(res) {
+				$paragraph.text(res.value.joke);
+			}
+		});
+	}
+
+	getJoke(); 
+});
