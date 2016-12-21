e640970e7cda1687486f501d207a005ca46fb9b4
tomsloni <aiki.waw@gmail.com>
2016-12-21 02:33:13 +0100

    changes

--- greatquotes.js
+++ greatquotes.js
@@ -1,29 +1,32 @@
-﻿var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?';
-var tweetLink = "https://twitter.com/intent/tweet?text=";
+﻿$(function(){ 
+	var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?';
+	var tweetLink = "https://twitter.com/intent/tweet?text=";
 
-var $button = $('#generate').click(function() {
-	getQuote();
-});
-
-function getQuote() {
-	$.getJSON(url, createTweet);
-}
-
-function createTweet(input) {
-	if (!input.quoteAuthor.length) {
-		input.quoteAuthor = 'Unknown author';
-	}
-
-	var tweetText = 'Quote of the day - ' + input.quoteText + ' Author: ' + input.quoteAuthor;
-
-	if (tweetText.length > 140) {
+	$('#generate').trigger('click');
+	$('#generate').on('click', function() {
 		getQuote();
-	} else {
-		var tweet = tweetLink + encodeURIComponent(tweetText);
-		$('#quote').text(input.quoteText);
-		$('#author').text(input.quoteAuthor);
-		$('#tweet').attr('href', tweet)
-	}
-}
+	});
 
-$(function(){ getQuote(); });
+	function getQuote() {
+		$.getJSON(url, createTweet);
+	}
+
+	function createTweet(input) {
+		if (!input.quoteAuthor.length) {
+			input.quoteAuthor = 'Unknown author';
+		}
+
+		var tweetText = 'Quote of the day - ' + input.quoteText + ' Author: ' + input.quoteAuthor;
+
+		if (tweetText.length > 140) {
+			getQuote();
+		} else {
+			var tweet = tweetLink + encodeURIComponent(tweetText);
+			$('#quote').text(input.quoteText);
+			$('#author').text(input.quoteAuthor);
+			$('#tweet').attr('href', tweet);
+		}
+	}
+
+	getQuote(); 
+});
